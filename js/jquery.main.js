e1e757225e44910e9d723e370142dcb734b5f65d
Oleg <gvozdyk23@gmail.com>
2019-10-11 13:02:02 +0300

    rwd

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,10 @@
 $(function() {
     $('.search_btn').on('click', function() {
         $('.search_panel').toggleClass('search_active');
+    });
+
+    $('.burger').on('click', function() {
+        $('.main_menu').toggleClass('menu_active');
+        $(this).toggleClass('burger_active');
     })
 });
